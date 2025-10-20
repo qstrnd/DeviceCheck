84adf44ad4cfa38fdf84b674074c421748f030ce
Andy <qstrnd@pm.me>
2025-10-20 20:35:40 +0200

    Initial commit

---
+++ DeviceCheckApp/DeviceCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeviceCheck
+//
+//  Created by Andy on 20.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeviceCheckApp/DeviceCheck/DeviceCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceCheckApp.swift
+//  DeviceCheck
+//
+//  Created by Andy on 20.10.25.
+//
+
+import SwiftUI
+
+@main
+struct DeviceCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
